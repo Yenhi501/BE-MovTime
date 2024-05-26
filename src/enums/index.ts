5834f6870a59407c0cc7f2e27a725ee5d1c840e4
yennhi <yennhi81lqs@gmail.com>
2024-05-26 22:20:58 +0700

    📝 update file enums

---
+++ src/enums/index.ts
@@ -0,0 +1,16 @@
+export enum UrlService {
+	sandbox = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+}
+
+export enum VnpCurrCode {
+	VND = 'VND',
+}
+
+export enum VnpLocale {
+	VN = 'vn',
+	EN = 'en',
+}
+
+export enum VnpOrderType {
+	OTHER = 'other',
+}
